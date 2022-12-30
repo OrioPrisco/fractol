1404dcdf67c878252451c5b8af415a79d071e467
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-12-30 11:49:30 +0100

    env.c : Norm

--- srcs/env.c
+++ srcs/env.c
@@ -14,10 +14,21 @@
 #include "fractol.h"
 #include "libft.h"
 
+static int	init_img(t_env *env, t_img *img, int width, int height)
+{
+	img->img = 0;
+	img->img = mlx_new_image(env->mlx, width, height);
+	if (!img->img)
+		return (1);
+	img->data = (unsigned char *)mlx_get_data_addr(img->img,
+			&img->bits_per_pixel, &img->line_length, &img->endian);
+	img->height = height;
+	img->width = width;
+	return (0);
+}
+
 int	init_env(t_env *env, t_img *img)
 {
-	t_img	*cam_img;
-
 	env->win = 0;
 	env->mlx = mlx_init();
 	img->img = 0;
@@ -30,21 +41,11 @@ int	init_env(t_env *env, t_img *img)
 		return (2);
 	env->camera.iter = 3;
 	env->camera.scale = 400;
-	env->camera.center = complex(0,0);
+	env->camera.center = complex(0, 0);
 	env->debug = 0;
-	env->camera.work_buffer.img = mlx_new_image(env->mlx, WIDTH, HEIGHT);
-	img->img = mlx_new_image(env->mlx, WIDTH, HEIGHT);
-	if (!img->img || ! env->camera.work_buffer.img)
+	if (init_img(env, img, WIDTH, HEIGHT)
+		|| init_img(env, &env->camera.work_buffer, WIDTH, HEIGHT))
 		return (1);
-	img->data = (unsigned char *)mlx_get_data_addr(img->img,
-			&img->bits_per_pixel, &img->line_length, &img->endian);
-	cam_img = &env->camera.work_buffer;
-	cam_img->data = (unsigned char *)mlx_get_data_addr(cam_img->img,
-			&cam_img->bits_per_pixel, &cam_img->line_length, &cam_img->endian);
-	img->height = HEIGHT;
-	img->width = WIDTH;
-	cam_img->width = WIDTH;
-	cam_img->height = HEIGHT;
 	update_camera(&env->camera);
 	return (0);
 }
